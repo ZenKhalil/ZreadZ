0aea10dcfa3893b83707f7dacccb53cf2e46bee8
ZenKhalil <feattzen@gmail.com>
2023-12-21 15:42:40 +0100

    added the header, nav and links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
+import { useState, useEffect } from "react"
+import Nav from "./components/Nav"
+import Header from "./components/Header"
+import Feed from "./components/Feed"
+import PopUp from "./components/PopUp"
+
 const App = () => {
+  const [ user, setUser] = useState(null)
+
+  const userId = "ee100716-70ed-491d-b4d4-df126ae9ddfc"
+
+  const getUser = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/users?user_uuid=${userId}`)
+      const data = await response.json()
+      setUser(data[0])
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getUser()
+  }, [])
+
+  console.log(user)
+
   return (
-    <div className="app">
-    </div>
-  )
+    <>
+      {user && <div className="app">
+        <Nav url={user.instagram_url}/>
+        <Header 
+          user={user}
+        />
+        <Feed />
+        {/*<PopUp/>*/}
+      </div>}
+    </>
+  );
 }
 
 export default App
---
+++ src/components/PopUp.js
@@ -0,0 +1,13 @@
+import PopUpThread from "./PopUpThreads";
+import ThreadInput from "./ThreadInput";
+
+const PopUp = () => {
+  return (
+  <div className="popup">
+    <PopUpThread/>
+    <ThreadInput/>
+  </div>
+  )
+}
+
+export default PopUp;
